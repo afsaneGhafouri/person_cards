56e19af4b96c20ca88da677bd2f3c05793595aa5
afsaneGhafouri <JUnia6468>
2019-09-16 15:01:51 +1000

    state update lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,10 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        console.log('[App.js] constructor');
+    }
 
     state = {
         persons: [
@@ -15,6 +19,15 @@ class App extends Component {
         otherState: 'some other value',
         showPersons: false
     };
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('[App.js] getDerivedStateFromProps', props);
+        return state;
+    }
+
+    componentDidMount() {
+        console.log('[App.js] componentDidMount');
+    }
 
     nameChangedHandler = (event, id) => {
         const personIndex = this.state.persons.findIndex(p => {
@@ -48,6 +61,7 @@ class App extends Component {
     };
 
     render() {
+        console.log('[App.js] render');
         let persons = null;
         if (this.state.showPersons) {
             persons = <Persons
@@ -60,6 +74,7 @@ class App extends Component {
         return (
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.title}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     toggled={this.togglePersonsHandler}
